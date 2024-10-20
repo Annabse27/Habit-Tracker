356891ed99d53b7d918321f92b07246142e8b881
Annabse27 <annabse27@gmail.com>
2024-10-20 19:08:45 +0200

    fix: Добавлены проверки доступа к привычкам других пользователей.

--- habits/views.py
+++ habits/views.py
@@ -3,6 +3,8 @@ from .models import Habit
 from .serializers import HabitSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 class HabitViewSet(viewsets.ModelViewSet):
@@ -19,8 +21,24 @@ class HabitViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
     def perform_update(self, serializer):
+        habit = serializer.instance
+        if habit.user != self.request.user:
+            return Response({"detail": "Вы не можете редактировать чужие привычки."}, status=status.HTTP_403_FORBIDDEN)
         print(f'Updating habit for user: {self.request.user}')
         serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        habit = get_object_or_404(Habit, id=instance.id)
+        if habit.user != self.request.user:
+            return Response({"detail": "Вы не можете удалять чужие привычки."}, status=status.HTTP_403_FORBIDDEN)
+        habit.delete()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.user != request.user:
+            return Response({"detail": "Вы не можете просматривать чужие привычки."}, status=status.HTTP_403_FORBIDDEN)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     @action(detail=False, methods=['get'], permission_classes=[permissions.AllowAny])
     def public(self, request):
